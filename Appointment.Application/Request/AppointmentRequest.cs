66df4a7bac17ba2a2fc271ffa084d328a83959aa
IcaroTimoteo96 <timoteoicaro18@gmail.com>
2025-02-19 22:01:24 -0300

    inserção de prop Name

--- Appointment.Application/Request/AppointmentRequest.cs
+++ Appointment.Application/Request/AppointmentRequest.cs
@@ -2,6 +2,7 @@
 {
     public class AppointmentRequest
     {
+        public required string Name { get; set; }
         public required string Email { get; set; }
 
         public required string Phone { get; set; }
